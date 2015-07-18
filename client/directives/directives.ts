a575ede0bf9b2fd4d169091f794a72935b6e183f
Jeremy Wilken <gnomation@gnomeontherun.com>
2015-07-17 20:38:22 -0500

    Made XLarge its own module

--- client/directives/directives.ts
+++ client/directives/directives.ts
@@ -1,6 +1,8 @@
 /// <reference path="../../typings/tsd.d.ts" />
 import {Autofocus} from './Autofocus';
+import {XLarge} from './XLarge';
 
 export var appDirectives: Array<Type> = [
-  Autofocus
+  Autofocus,
+  XLarge
 ];
